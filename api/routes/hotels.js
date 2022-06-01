864710b5dcc7c5179b93d55e6730e82f24a8bb46
yzeren <yilmazzeren93@gmail.com>
2022-06-01 16:24:45 +0300

    Bug fix

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -21,7 +21,7 @@ router.put("/:id", verifyAdmin, updateHotel);
 router.delete("/:id", verifyAdmin, deleteHotel);
 
 // GET
-router.get("/find:id", getHotel);
+router.get("/find/:id", getHotel);
 
 // GET ALL
 router.get("/", getAllHotels);
