d055f0ad1bb71e511adcb82ac514f2066268c1a3
yzeren <yilmazzeren93@gmail.com>
2022-06-01 11:02:16 +0300

    Some package install. Create react custom hook. Api updated

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <div className="h-12 bg-blue-700 flex justify-center">
       <div className="w-full max-w-5xl text-white flex items-center justify-between">
-        <span className="font-medium">Booking</span>
+        <Link to="/">
+          <span className="font-medium">Booking</span>
+        </Link>
         <div className="navItems">
           <button className="ml-5 py-1 px-2 cursor-pointer font-medium hover:scale-110 duration-100 transition-all border rounded-xl bg-blue-400">
             Register
--- client/src/components/propertyList/PropertyList.jsx
+++ client/src/components/propertyList/PropertyList.jsx
@@ -1,61 +1,43 @@
+import { Fragment } from "react";
+import useFetch from "../../hooks/useFetch";
+
 export default function PropertyList() {
+  const { data, loading, error } = useFetch("hotels/countByType");
+  const images = [
+    "https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o=",
+    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg",
+    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/bg_resorts/6f87c6143fbd51a0bb5d15ca3b9cf84211ab0884.jpg",
+    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-villas_300/dd0d7f8202676306a661aa4f0cf1ffab31286211.jpg",
+    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-chalet_300/8ee014fcc493cb3334e25893a1dee8c6d36ed0ba.jpg",
+  ];
+  console.log(data);
   return (
     <div className="w-full max-w-5xl flex justify-between gap-5">
-      <div className="flex-1 rounded-lg overflow-hidden cursor-pointer">
-        <img
-          src="https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o="
-          alt=""
-          className="w-full h-36 object-cover hover:scale-105 duration-200 hover:cursor-pointer"
-        />
-        <div>
-          <h1>Hotels</h1>
-          <h1 className="text-sm text-gray-500">233</h1>
-        </div>
-      </div>
-      <div className="flex-1 rounded-lg overflow-hidden cursor-pointer">
-        <img
-          src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg"
-          alt=""
-          className="w-full h-36 object-cover hover:scale-105 duration-200 hover:cursor-pointer"
-        />
-        <div>
-          <h1>Apartments</h1>
-          <h1 className="text-sm text-gray-500">233</h1>
-        </div>
-      </div>
-      <div className="flex-1 rounded-lg overflow-hidden cursor-pointer">
-        <img
-          src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-villas_300/dd0d7f8202676306a661aa4f0cf1ffab31286211.jpg"
-          alt=""
-          className="w-full h-36 object-cover hover:scale-105 duration-200 hover:cursor-pointer"
-        />
-        <div>
-          <h1>Resorts</h1>
-          <h1 className="text-sm text-gray-500">233</h1>
-        </div>
-      </div>
-      <div className="flex-1 rounded-lg overflow-hidden cursor-pointer">
-        <img
-          src="https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o="
-          alt=""
-          className="w-full h-36 object-cover hover:scale-105 duration-200 hover:cursor-pointer"
-        />
-        <div>
-          <h1>Villas</h1>
-          <h1 className="text-sm text-gray-500">233</h1>
-        </div>
-      </div>
-      <div className="flex-1 rounded-lg overflow-hidden cursor-pointer">
-        <img
-          src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/bg_country_house/6ffa05069b50124c993f00c998f1dfc66999c93f.jpg"
-          alt=""
-          className="w-full h-36 object-cover hover:scale-105 duration-200 hover:cursor-pointer"
-        />
-        <div>
-          <h1>Cabins</h1>
-          <h1 className="text-sm text-gray-500">233</h1>
-        </div>
-      </div>
+      {loading ? (
+        "loading"
+      ) : (
+        <Fragment>
+          {data &&
+            images.map((img, i) => (
+              <div
+                key={i}
+                className="flex-1 rounded-lg overflow-hidden cursor-pointer"
+              >
+                <img
+                  src={img}
+                  alt=""
+                  className="w-full h-36 object-cover hover:scale-105 duration-200 hover:cursor-pointer"
+                />
+                <div>
+                  <h1 className="capitalize font-bold">{data[i]?.type}</h1>
+                  <h1 className="text-sm text-gray-500">
+                    {data[i]?.count} {data[i]?.type}
+                  </h1>
+                </div>
+              </div>
+            ))}
+        </Fragment>
+      )}
     </div>
   );
 }
